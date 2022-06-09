fa523decef6b1db71f11183ff0befc29c2e80bd9
BigsizeJ <apacjessie@gmail.com>
2022-06-09 16:03:46 +0800

    Making website responsive [UNFINISHED]

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const createAccount = document.querySelector('.btn')
 const pw = document.querySelector('#password')
 const rpw = document.querySelector('#confirm_pw')
+const Firstname = document.querySelector('#name');
 
-createAccount.addEventListener('click', (e) => {
-    if(pw.value.toString() !== rpw.value.toString()){
-        console.log("PASSWORD DOEST NOT MATCH!")
-    }
-    e.preventDefault()
-})
+
